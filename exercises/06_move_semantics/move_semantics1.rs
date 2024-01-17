08d0596549a2d8fe8c01126af15bd49c2ca04233
Just2TryThis <79096855+Just2TryThis@users.noreply.github.com>
2024-01-17 22:42:22 +0100

    Vec fonction into mutable

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -3,7 +3,24 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// Ca fonctionne 
+
+//#[test]
+//fn main() {
+//    let vec0 = vec![22, 44, 66]
+//
+//    let vec1 = fill_vec(vec0);
+//
+//    assert_eq!(vec1, vec![22, 44, 66, 88]);
+//}
+//
+//fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+//    let mut x = vec.clone();
+//
+//    vec.push(88);
+//
+//    vec
+//}
 
 #[test]
 fn main() {
@@ -15,7 +32,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
