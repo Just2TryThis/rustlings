4607924b0f1e45cac68a38b3ab56f615a335be15
Just2TryThis <79096855+Just2TryThis@users.noreply.github.com>
2024-01-17 22:34:46 +0100

    Mult Interieur elem // collect produit

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -7,13 +7,21 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+#[warn(dead_code)]
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        //for elem in x.iter_mut() {
+        //    *elem += 2;
+        //    }
+        //    assert_eq!(x, &[3, 4, 6]);
+        //    }
+        //let i2:i32= *i;
+        //println!("{}",i2);
+        //i =i*2
+        //println!("{}",i);
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -24,7 +32,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element * 2
     }).collect()
 }
 
